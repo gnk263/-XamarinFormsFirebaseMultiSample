c236356fbf4fa30a7a08a1e2dcd8f80117cee92d
gnk263 <gnk263@users.noreply.github.com>
2019-07-14 21:24:54 +0900

    add test code for android

--- XFFirebaseMultiSample.Android/MainActivity.cs
+++ XFFirebaseMultiSample.Android/MainActivity.cs
@@ -1,11 +1,9 @@
-﻿using System;
-
-using Android.App;
+﻿using Android.App;
 using Android.Content.PM;
 using Android.Runtime;
-using Android.Views;
-using Android.Widget;
 using Android.OS;
+
+using Firebase.Analytics;
 
 namespace XFFirebaseMultiSample.Droid
 {
@@ -17,11 +15,19 @@ namespace XFFirebaseMultiSample.Droid
             TabLayoutResource = Resource.Layout.Tabbar;
             ToolbarResource = Resource.Layout.Toolbar;
 
+            var analytics = FirebaseAnalytics.GetInstance(this);
+
             base.OnCreate(savedInstanceState);
 
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
+
+            var bundle = new Bundle();
+            bundle.PutString(FirebaseAnalytics.Param.ItemCategory, "Monday");
+            bundle.PutString(FirebaseAnalytics.Param.ItemName, "21:54");
+            bundle.PutInt(FirebaseAnalytics.Param.ItemId, 2154);
+            analytics.LogEvent(FirebaseAnalytics.Event.SelectContent, bundle);
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
